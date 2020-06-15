b4bdc8f912b1e40f164496c74d240612787ef82a
travis Lawson <travislawclimb@gmail.com>
2020-06-15 02:26:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,7 @@
 class Artist
   
   @@all = [] 
+  @@song_count = 0
   
   attr_accessor :name
   
@@ -13,7 +14,7 @@ class Artist
   def add_song(song) 
     @songs << song 
     song.artist = self
-    
+    @@song_count +=1 
   end   
   
   def add_song_by_name(song_name)
